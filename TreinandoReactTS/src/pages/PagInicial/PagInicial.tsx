25e50a5a8360e3aca0b998033c36e89a2d0fb405
Reginaldo <reginaldolgj@gmail.com>
2023-06-09 21:03:50 -0300

    criando o crud no react.

--- TreinandoReactTS/src/pages/PagInicial/PagInicial.tsx
+++ TreinandoReactTS/src/pages/PagInicial/PagInicial.tsx
@@ -1,33 +1,65 @@
 import { useCallback, useState } from "react";
 
+interface ITarefa {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+}
 export const PagInicial = () => {
-  const [lista, setLista] = useState<string[]>([""]);
-const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
-  if(e.key === 'Enter') {
-    if(e.currentTarget.value.trim().length === 0) return;
-    //forma 1 de inserir itens na lista
-    // setLista([...lista, e.currentTarget.value]);
+  const [lista, setLista] = useState<ITarefa[]>([]);
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      if (e.key === "Enter") {
+        if (e.currentTarget.value.trim().length === 0) return;
+        //forma 1 de inserir itens na lista
+        // setLista([...lista, e.currentTarget.value]);
 
-    const value = e.currentTarget.value;
-    e.currentTarget.value = '';
-    setLista((oldLista) => {
-      return [...lista, value];
-    });
-  } 
-  return(
-    <h1>aol</h1>
-  )
-}, [lista]);
+        const value = e.currentTarget.value;
+
+        e.currentTarget.value = "";
+
+        setLista((oldLista) => {
+          if (oldLista.some((ListItem) => ListItem.title === value)) return oldLista;
+
+          return [
+            ...oldLista,
+            {
+              id: oldLista.length,
+              title: value,
+              isCompleted: false,
+            },
+          ];
+        });
+      }
+    }, []);
 
   return (
     <>
       <p>Lista</p>
-      <input 
-        onKeyDown={handleInputKeyDown}
-      />
+      <input onKeyDown={handleInputKeyDown} />
+      <p>{lista.filter((ListItem) => ListItem.isCompleted).length}</p>
       <ul>
-        {lista.map((value, index) => {
-        return <li key={index}>{value}</li>;
+        {lista.map((ListItem) => { return <li key={ListItem.id}>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  setLista((oldLista) => {
+                    return oldLista.map((oldListItem) => {
+                      const newIsSelected =
+                        oldListItem.title === ListItem.title
+                          ? !oldListItem.isCompleted
+                          : oldListItem.isCompleted;
+
+                      return {
+                        ...oldListItem,
+                        isSelect: newIsSelected,
+                      };
+                    });
+                  });
+                }}
+              />
+              {ListItem.title}
+            </li>
         })}
       </ul>
     </>
---
+++ TreinandoReactTS/src/services/api/tarefas/tarefasService.ts
@@ -0,0 +1,57 @@
+import { Api } from "../ApiContext";
+import { ApiException } from "../ApiException";
+
+interface ITarefa {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+}
+
+const  getAll = async (): Promise<ITarefa[] | ApiException> => {
+  try {
+    const {data} = await Api().get('/tarefas');
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao consultar API.");
+  }
+};
+const getById = async (id: number): Promise<ITarefa | ApiException> => {
+  try {
+    const {data} = await Api().get(`/tarefa/${id}`);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao consultar API.");
+  }
+};
+const create = async (dataToCreate: Omit<ITarefa, "id">): Promise<ITarefa | ApiException> => {
+  try {
+    const {data} = await Api().post<any>('/tarefas', dataToCreate);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao criar o registro.");
+  }
+};
+const updateById = async (id: string, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
+  try {
+    const {data} = await Api().put<any>(`/tarefas${id}`, dataToUpdate);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao atualizar o registro.");
+  }
+};
+const deleteById = async (id: string): Promise<undefined | ApiException> => {
+  try {
+    const {data} = await Api().get(`/tarefas${id}`);
+    return undefined;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao apagar API.");
+  }
+};
+
+export const TarefasService = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+};
